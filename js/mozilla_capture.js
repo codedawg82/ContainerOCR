b404576bda4b9ed78e4aa17ad0a4b040ab757fe5
Elvis DSouza <Elvis.DSouza@tomra.com>
2017-01-06 15:50:07 -0500

    Modified code to send images to Computer Vision

--- js/mozilla_capture.js
+++ js/mozilla_capture.js
@@ -11,6 +11,7 @@
             if (files && files.length > 0) {
                 file = files[0];
                 try {
+
                     // Get window.URL object
                     var URL = window.URL || window.webkitURL;
 
@@ -24,6 +25,39 @@
                     showPicture.onload = function () {
                         URL.revokeObjectURL(imgURL);
                     };
+
+                    console.log("Preparing HTTP POST request");
+
+                    // Send file to ComputerVision
+                    var http = new XMLHttpRequest();
+                    var urlVision = "https://api.projectoxford.ai/vision/v1.0/ocr?language=en&detectOrientation=true";
+                    //var contentType = "multipart/form-data";
+                    var contentType = "application/octet-stream";
+                    var apikey = "c43bf3b1fd0e413cbea48f33b693cf0f";
+
+                    var fd = new FormData();
+                    fd.append("myimg", file);
+
+
+                    http.open("POST", urlVision, true);
+                    http.setRequestHeader('Content-Type', contentType);
+                    http.setRequestHeader('Ocp-Apim-Subscription-Key', apikey);
+
+                    http.onreadystatechange = function () {
+                        //Call a function when the state changes.
+                        if (http.readyState == 4 && http.status == 200) {
+                            alert(http.responseText);
+                        } else {
+                            var ocr_text = document.querySelector("#ocr_text");
+                            ocr_text.innerHTML = http.responseText;
+                        }
+                    }
+
+                    console.log("sending request");
+                    console.log(imgURL);
+
+                    http.send(file);
+
                 }
                 catch (e) {
                     try {
